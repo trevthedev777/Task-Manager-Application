0d4f3013f4cfcab547f60011b5e3cd83e6da8543
trevthedev777 <bloodsportrecords@gmail.com>
2022-08-09 05:14:48 +0000

    fix bug not initialising the collapsable mobile nav menu

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,7 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Initialize Nav Menu
-    let sidenav = document.querySelectorAll(".sidenav");
-    M.Sidenav.init(sidenav);
-  });
-
-  console.log('hello word')
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
